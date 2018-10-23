d4bb5e1b73beae2ceface7dd48ec42d01e3c6e0f
lihongli <lihognli@u51.com>
2018-10-23 20:17:31 +0800

    init

---
+++ src/main/java/com/li/myspringlearning/MyspringlearningApplication.java
@@ -0,0 +1,12 @@
+package com.li.myspringlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringlearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringlearningApplication.class, args);
+	}
+}
